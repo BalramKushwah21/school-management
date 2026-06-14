7c45f374b3e9b6935f4823665d5629eda9523635
Balram <bkushwah4321@gmail.com>
2026-06-14 10:32:28 +0530

    We will start again

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import "./globals.css";
-import Navbar from "@/components/navbar/navbar";
-import { AuthProvider } from "@/auth/auth-provider";
-import RouteGuard from "@/auth/route-guard";
+
 
 export const metadata = {
   title: "School Management",
